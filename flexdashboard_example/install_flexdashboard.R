c843aae6cd1332c8575209a699b4b5b45ff40c2c
Jaakko Talonen <jaakko.talonen@napa.fi>
2020-09-16 18:31:43 +0300

    init commit

---
+++ flexdashboard_example/install_flexdashboard.R
@@ -0,0 +1,4 @@
+# https://ramikrispin.github.io/2020/09/deploying-flexdashboard-on-github-pages/
+install.packages('flexdashboard')
+library(flexdashboard)
+# Note that you probably won’t have this template available on this template menu if the flexdashboard package is not installed
